d173592a5db9735e5d15fce5f2b843cde299c5ef
Pravin Deshatwar <pravindeshatwar@gmail.com>
2018-05-02 17:29:49 +0530

    same code

--- js/custom.js
+++ js/custom.js
@@ -2,37 +2,48 @@ $(function () {
 
     // animate on scroll
     new WOW().init();
+    $('.counter').counterUp({
+      delay:10,
+      time: 3000
+    });
+    $(".image_slider").slick({
+    	dots: true,
+       arrows: true,
+       autoplay: true
+    });
+
 });
+
+$(function(){
+   $("#customer-testimonials").owlCarousel({
+      items: 1,
+      autoplay: true,
+      loop: true,
+      smartSpeed: 700,
+      autoplayHoverPause: true
+   });
+})
+
 
 /*   stats    */
 $(function(){
-	$('.counter').counterUp({
-		delay:10,
-		time: 3000
-	})
+
 })
 
 /* slider */
 
 $(document).ready(function(){
 
-  $('.fade').slick({
-   dots: true
-  });
+
 });
 
 
 $(document).ready(function(){
-   $(".image_slider").slick({
-   	dots: true,
-      arrows: true,
-      // autoplay: true
-   });
+
 });
 $(document).ready(function(){
-   $(".image_slider_topper").slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-   });
+
 });
+
+$(function(){
+})
